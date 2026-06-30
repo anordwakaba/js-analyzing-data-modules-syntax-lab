d0f9ecd802474415b8c3eaade3b28c7af289c435
Your Name <you@example.com>
2026-06-30 22:35:55 +0300

    1st_compl

--- index.js
+++ index.js
@@ -1,3 +1,33 @@
+// Load the datejs package
+require("datejs");
+
+// Function to combine multiple arrays of usernames
+function combineUsers(...args) {
+
+    // Object that will be returned
+    const combinedObject = {
+        users: []
+    };
+
+    // Go through each array of usernames
+    for (const usernames of args) {
+
+        // Merge the current array into the existing users array
+        combinedObject.users = [
+            ...combinedObject.users,
+            ...usernames
+        ];
+    }
+
+    // Save today's date
+    combinedObject.merge_date = Date.today().toString("M/d/yyyy");
+
+    // Return the completed object
+    return combinedObject;
+}
+
+// Export the function
+module.exports = combineUsers;
 
 
 
